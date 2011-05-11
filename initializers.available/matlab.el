8bce6dac0c26a0a420a0150462d0c14f9d32504c
Ben Mabey <ben@benmabey.com>
2011-05-10 22:24:54 -0600

    shift-enter goodness for matlab (same for ess/r mode)

--- initializers.available/matlab.el
+++ initializers.available/matlab.el
@@ -1,2 +1,6 @@
 (add-to-list 'load-path "~/.emacs.d/vendor/matlab-emacs")
 (load-library "matlab-load")
+
+(add-hook 'matlab-mode
+          '(lambda()
+             (local-set-key [(shift return)] 'matlab-shell-run-region-or-line)))
