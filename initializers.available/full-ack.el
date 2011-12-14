9fc49e3e6b35c53b7d93068b50bee1bdea77e080
Tim Harper <timcharper@gmail.com>
2011-12-14 14:29:24 -0700

    define new command, ack-here, which runs from the pwd of the current buffer

--- initializers.available/full-ack.el
+++ initializers.available/full-ack.el
@@ -1,1 +1,7 @@
+(defun ack-here ()
+  "Run ack from default-directory of the current buffer"
+  (interactive)
+  (require 'full-ack)
+  (ack-run default-directory t (ack-read t)))
+
 (global-set-key [(shift super a)] 'ack)
