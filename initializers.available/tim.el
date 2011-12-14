5c240bfc36c5c571ddbe60dc6f94bff0ba5164e6
Tim Harper <timcharper@gmail.com>
2011-12-14 14:34:27 -0700

    new dired feature - F to create empty file

--- initializers.available/tim.el
+++ initializers.available/tim.el
@@ -78,3 +78,15 @@
 
 (global-set-key (kbd "C-c +") 'increment-number-at-point)
 (global-set-key (kbd "C-c -") 'decrement-number-at-point)
+
+(defun dired-create-file (file)
+  "Create an empty file called FILE."
+  (interactive
+   (list (read-file-name "Touch file: " (dired-current-directory))))
+  (let ((expanded (expand-file-name file)))
+    (message "%s" (shell-command-to-string (format "%s %S" dired-touch-program expanded)))
+    (dired-add-file expanded)
+    (dired-move-to-filename)))
+
+(eval-after-load 'dired
+  '(define-key dired-mode-map (kbd "F") 'dired-create-file))
