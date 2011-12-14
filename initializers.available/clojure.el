a10dab0d7992923b369ac493298bb2103b7bd6ec
Tim Harper <timcharper@gmail.com>
2011-12-14 14:29:00 -0700

    don't assume clojure is already loaded

--- initializers.available/clojure.el
+++ initializers.available/clojure.el
@@ -1,12 +1,4 @@
 (add-hook 'clojure-mode-hook (lambda ()
-                               (define-clojure-indent
-                                 (describe 1)
-                                 (it 1)
-                                 (def 1)
-                                 (condp 2)
-                                 (cond 0))))
-
-(add-hook 'clojure-mode-hook (lambda ()
                                (highlight-parentheses-mode)))
 
 (add-hook 'emacs-lisp-mode-hook (lambda () (highlight-parentheses-mode)))
@@ -14,3 +6,9 @@
 (add-hook 'slime-repl-mode-hook 'clojure-mode-font-lock-setup)
 
 (eval-after-load 'slime '(setq slime-protocol-version 'ignore)) ; prevents warning
+(eval-after-load 'clojure-mode '(define-clojure-indent
+                                 (describe 1)
+                                 (it 1)
+                                 (def 1)
+                                 (condp 1)
+                                 (cond 0)))
