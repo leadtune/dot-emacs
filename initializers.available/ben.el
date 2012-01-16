f3ffeb6517d55843081e1cf3ca8ed4b4269fcfc5
Ben Mabey <ben@benmabey.com>
2012-01-16 09:55:40 -0700

    turn paredit-mode on in slime-mode

--- initializers.available/ben.el
+++ initializers.available/ben.el
@@ -1,4 +1,5 @@
 (add-hook 'slime-repl-mode-hook 'viper-mode)
+(add-hook 'slime-repl-mode-hook 'paredit-mode)
 
 ;; gracias a http://www.emacswiki.org/emacs/DeletingWhitespace#toc3
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
