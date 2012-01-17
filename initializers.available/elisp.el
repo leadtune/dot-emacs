1e97a28d3bbdfdcb165452ee2fab289d16a1f186
Ben Mabey <ben@benmabey.com>
2012-01-17 11:08:46 -0700

    fixes clojure mode to use vendor esk-lisp and breaks out elisp customizations

--- initializers.available/elisp.el
+++ initializers.available/elisp.el
@@ -1,13 +1,9 @@
 (ensure-packages-installed
   '(elisp-slime-nav ;; remove once we can add starter-kit back in...
     ;; starter-kit-lisp ;; comment in once new starter-kit has been pushed to marmalade
-       clojure-mode midje-mode clojure-test-mode slime
           highlight-parentheses))
 
 (require 'starter-kit-lisp)
 (require 'highlight-parentheses)
 
-(add-hook 'clojure-mode-hook 'highlight-parentheses-mode)
-(add-hook 'slime-repl-mode-hook 'highlight-parentheses-mode)
-
-(add-hook 'slime-repl-mode-hook 'clojure-mode-font-lock-setup)
+(add-hook 'emacs-lisp-mode-hook (lambda () (highlight-parentheses-mode)))
